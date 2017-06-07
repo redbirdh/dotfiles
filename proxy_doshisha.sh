2460965c1e7e2fae54d5b6368facdf3e933a42cd
redbirdh <string.d7@gmail.com>
2017-06-07 15:56:34 +0900

    add zshrc setting

---
+++ proxy_doshisha.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export HTTP_PROXY=bup0093:Sn0wscape@http://proxyr.drm.doshisha.ac.jp:8080
+export HTTPS_PROXY=bup0093:Sn0wscape@https://proxyr.drm.doshisha.ac.jp:8080
+export http_proxy=bup0093:Sn0wscape@http://proxyr.drm.doshisha.ac.jp:8080
+export https_proxy=bup0093:Sn0wscape@https://proxyr.drm.doshisha.ac.jp:8080
+#export proxy-user=bup0093:Sn0wscape
+#export proxy=http://proxyr.drm.doshisha.ac.jp:8080
---
+++ proxy_unset.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset http_proxy
+unset https_proxy
+#unset proxy-user
+#unset proxy
