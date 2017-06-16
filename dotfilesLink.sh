d399ccc495a9f66bbbe511e66f32fc4b6cfdc282
redbirdh <string.d7@gmail.com>
2017-06-16 20:34:39 +0900

    vimのパッケージマネジャ入れてsyntastic導入したし.vimフォルダごとdotfilesで管理することに

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
